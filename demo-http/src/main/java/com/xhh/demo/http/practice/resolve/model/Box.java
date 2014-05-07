89db90fcf6b081174414052afe2360bda9e44bdd
lvyianran <xiahaihu213@163.com>
2014-05-07 22:08:26 +0800

    submit modify

---
+++ demo-http/src/main/java/com/xhh/demo/http/practice/resolve/model/Box.java
@@ -0,0 +1,26 @@
+package com.xhh.demo.http.practice.resolve.model;
+
+import lombok.*;
+
+/**
+ * Created by tiger on 14-4-24.
+ */
+public class Box {
+
+    @Getter
+    @Setter
+    private String id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String size;
+
+    @Getter
+    @Setter
+    private String color;
+
+}
---
+++ demo-http/src/main/java/com/xhh/demo/http/practice/resolve/model/PencilBox.java
@@ -0,0 +1,16 @@
+package com.xhh.demo.http.practice.resolve.model;
+
+import lombok.*;
+
+/**
+ * Created by tiger on 14-4-25.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PencilBox extends Box{
+
+    @Getter
+    @Setter
+    private String pencilBox;
+}

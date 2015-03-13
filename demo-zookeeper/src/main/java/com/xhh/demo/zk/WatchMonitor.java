0b6c9243049019cc519a6a729d56ed739dc23d39
lvyianran <xiahaihu213@163.com>
2015-03-13 16:51:07 +0800

    add demo-zookeeper

---
+++ demo-zookeeper/src/main/java/com/xhh/demo/zk/WatchMonitor.java
@@ -0,0 +1,141 @@
+package com.xhh.demo.zk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 监控 zk 目录下增、删、改的节点
+ *
+ * @author tiger
+ * @version 1.0.0 createTime: 15/3/12 下午11:59
+ * @since 1.6
+ */
+@Slf4j
+public class WatchMonitor implements Runnable {
+
+    /** curator zkClient */
+    private static CuratorFramework zkClient = null;
+
+    /** zkConnectionString */
+    private final static String ZK_CONN = "192.168.106.102:2181";
+
+    /** 监控结点 */
+    public static final String PATH = "/app1";
+
+    /** 监控结点子结点列表 */
+    private static List<String> nodeList;
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new WatchMonitor());
+        thread.start();
+    }
+
+    WatchMonitor () {
+        zkClient = CuratorFrameworkFactory.builder().connectString(ZK_CONN)
+                .sessionTimeoutMs(30000)
+                .connectionTimeoutMs(30000)
+                .canBeReadOnly(false)
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .namespace("cluster")
+                .defaultData(null)
+                .build();
+        try {
+            zkClient.getConnectionStateListenable().addListener(new ConnectionStateListener() {
+                @Override
+                public void stateChanged(CuratorFramework curatorFramework, ConnectionState connectionState) {
+                    log.debug("----------- connectionState.name: {}", connectionState.name());
+                }
+            });
+            zkClient.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        CuratorWatcher wc = new CuratorWatcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                // 结点数据改变之前的结点列表
+                List<String> nodeListBefore = nodeList;
+                // 主结点的数据发生改变时
+                if (watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged) {
+                    log.info("Node data changed:" + watchedEvent.getPath());
+                }
+                if (watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted){
+                    log.info("Node deleted:" + watchedEvent.getPath());
+                }
+                if(watchedEvent.getType()== Watcher.Event.EventType.NodeCreated){
+                    log.info("Node created:"+watchedEvent.getPath());
+                }
+
+                // 获取更新后的nodelist
+                try {
+                    nodeList = zkClient.getChildren().forPath(watchedEvent.getPath());
+                } catch (KeeperException e) {
+                    System.out.println(watchedEvent.getPath()+" has no child, deleted.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                List<String> nodeListNow = nodeList;
+                // 增加结点
+                if (nodeListBefore.size() < nodeListNow.size()) {
+                    for (String str : nodeListNow) {
+                        if (!nodeListBefore.contains(str)) {
+                            log.info("Node created:" + watchedEvent.getPath() + "/" + str);
+                        }
+                    }
+                }
+            }
+        };
+
+        /**
+         * 持续结点监控
+         */
+        while (true) {
+            try {
+                // 监控主结点
+                zkClient.checkExists().usingWatcher(wc);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (null == zkClient.checkExists().forPath(PATH)) {
+                    nodeList = new ArrayList<String>();
+                } else {
+                    nodeList = zkClient.getChildren().usingWatcher(wc).forPath(PATH);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // 对PATH下的每个结点都设置一个watcher
+            for (String nodeName : nodeList) {
+                try {
+                    zkClient.checkExists().usingWatcher(wc).forPath(PATH + "/" + nodeName);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            // sleep一会，减少CUP占用率
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ demo-zookeeper/src/main/java/com/xhh/demo/zk/ZkDataTest.java
@@ -0,0 +1,187 @@
+package com.xhh.demo.zk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.*;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * zk 测试入口
+ *
+ * <p>zk 测试例子
+ *
+ * @author tiger
+ * @version 1.0.0 createTime: 15/3/11 下午5:55
+ * @since 1.6
+ */
+@Slf4j
+public class ZkDataTest {
+
+    private static CuratorFramework zkClient = null;
+
+    private final static String ZK_CONN = "localhost:2181";
+
+    public static final String PATH = "/app1";
+
+    public static void main(String[] args) throws InterruptedException {
+        zkClient = CuratorFrameworkFactory.builder().connectString(ZK_CONN)
+                .sessionTimeoutMs(30000)
+                .connectionTimeoutMs(30000)
+                .canBeReadOnly(false)
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .namespace("cluster")
+                .defaultData(null)
+                .build();
+
+        try {
+            zkClient.getConnectionStateListenable().addListener(new ConnectionStateListener() {
+                @Override
+                public void stateChanged(CuratorFramework curatorFramework, ConnectionState connectionState) {
+                    log.debug("----------- connectionState.name: {}", connectionState.name());
+                }
+            });
+
+            zkClient.getCuratorListenable().addListener(new CuratorListener() {
+                @Override
+                public void eventReceived(CuratorFramework curatorFramework,
+                                          CuratorEvent curatorEvent) throws Exception {
+                    log.debug(".........  curatorEvent.type: {}", curatorEvent.getType().name());
+                }
+            });
+            zkClient.start();
+
+            log.debug("namespace: {}", zkClient.getNamespace());
+
+            test();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //Thread.sleep(5000);
+            CloseableUtils.closeQuietly(zkClient);
+            log.debug("zkClient stop!");
+        }
+    }
+
+    public static void test() throws Exception {
+        ZkDataTest zkDataTest = new ZkDataTest();
+        zkDataTest.getChildren(PATH);
+//        if (zkDataTest.checkExist(PATH)) {
+//            zkDataTest.delete(PATH);
+//        }
+//        zkDataTest.createrOrUpdate(PATH + "/a", "aaaa");
+        log.debug("namespace: {}", zkClient.getNamespace());
+        log.debug("start to getListChildren");
+//        zkDataTest.createrOrUpdate(PATH + "/a", "cccc");
+        zkDataTest.createEphemeral(PATH + "/aa");
+//        zkDataTest.createrOrUpdate(PATH + "/b", "1111");
+//        zkDataTest.createrOrUpdate("/app3", "333");
+//        zkDataTest.createrOrUpdate(PATH + "/c", "2222");
+//        zkDataTest.delete(PATH + "/a");
+//        zkDataTest.delete(PATH + "/b");
+    }
+
+    /**
+     * 创建或更新一个结点
+     *
+     * @param path 路径
+     * @param data 数据
+     * @throws Exception
+     */
+    public void createrOrUpdate(String path, String data) throws Exception {
+        zkClient.newNamespaceAwareEnsurePath(path).ensure(zkClient.getZookeeperClient());
+        zkClient.setData().forPath(path, data.getBytes());
+        log.debug("createOrUpdate, path: {}, data: {}", path, data);
+    }
+
+    public void createPersistent(String path) throws Exception {
+        zkClient.create().forPath(path);
+    }
+
+    public void createEphemeral(String path) throws Exception {
+        zkClient.create().withMode(CreateMode.EPHEMERAL).forPath(path);
+    }
+
+    /**
+     * 删除一个结点路径
+     *
+     * @param path 路径
+     * @throws Exception
+     */
+    public void delete(String path) throws Exception {
+        zkClient.delete().guaranteed().deletingChildrenIfNeeded().forPath(path);
+        log.debug("delete, path: {}", path);
+    }
+
+    /**
+     * 判断路径是否存在
+     *
+     * @param path 路径
+     * @throws Exception
+     */
+    public boolean checkExist(String path) throws Exception {
+        Stat stat = zkClient.checkExists().forPath(path);
+        if(stat == null) {
+            log.debug("checkExist, path: {} not exist", path);
+            return false;
+        } else {
+            log.debug("checkExist, stat: {}, path: {} already exist", stat, path);
+            return true;
+        }
+    }
+
+    /**
+     * 读取数据
+     *
+     * @param path 路径
+     * @throws Exception
+     */
+    public void read(String path) throws Exception {
+        String data = new String(zkClient.getData().forPath(path), "gbk");
+        log.debug("read, path: {}, data: {}", path, data);
+    }
+
+    /**
+     * 获取结点下的子路径
+     *
+     * @param path 路径
+     * @throws Exception
+     */
+    public void getChildren(String path) throws Exception {
+        if (!checkExist(path)) {
+            return;
+        }
+        List<String> paths = zkClient.getChildren().forPath(path);
+        for (String p : paths) {
+            String childPath = path + "/" + p;
+            log.debug("getListChildren, path: {}", childPath);
+            //delete(childPath);
+        }
+    }
+
+    /**
+     * 本地文件上传
+     *
+     * @param zkPath zk路径
+     * @param localpath 本地文件路径
+     * @throws Exception
+     */
+    public void upload(String zkPath, String localpath) throws Exception {
+        createrOrUpdate(zkPath, "");//创建路径
+        byte[] bs = FileUtils.readFileToByteArray(new File(localpath));
+        zkClient.setData().forPath(zkPath, bs);
+        log.debug("upload, zkPtch: {}, localpath: {}", zkPath, localpath);
+    }
+}
